7d8060fdf11c7bbb7efb3088f57bb2f73b7fda3c
gaseguraj <german.andres.segura@gmail.com>
2017-04-18 21:39:12 -0500

    modified:   WebContent/modules/leadCreation.jsp 	modified:   src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java 	modified:   src/edu/mum/mpp/zayagerman/controller/CreateClient.java

--- src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java
+++ src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java
@@ -1,10 +1,28 @@
 package edu.mum.mpp.zayagerman.businessLogic;
 
+import edu.mum.mpp.zayagerman.dao.ClientLeadDAOImpl;
+import edu.mum.mpp.zayagerman.entity.Client;
+import edu.mum.mpp.zayagerman.entity.ClientLead;
+import edu.mum.mpp.zayagerman.entity.ClientOpportunity;
+import edu.mum.mpp.zayagerman.service.ClientLeadDAO;
+import edu.mum.mpp.zayagerman.settings.ClientType;
+
 public class ClientFactory {
 	
-    public ClientFactory() {
-    	super();
+	private ClientLeadDAO dao;
+	
+    private ClientFactory() {
+    	dao = new ClientLeadDAOImpl();
     }
     
+    public static Client createClient(ClientType type){
+    	if(type.equals(ClientType.LEAD)){
+    		return new ClientLead();
+    	}
+    	if(type.equals(ClientType.OPPORTUNITY)){
+    		return new ClientOpportunity();
+    	}
+		return null;
+    }
     
 }
