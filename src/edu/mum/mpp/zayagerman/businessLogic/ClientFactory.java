1f3c0aa142ba6815e902c740a4e2174a4d145d34
gaseguraj <german.andres.segura@gmail.com>
2017-04-18 21:42:19 -0500

    modified:   src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java

--- src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java
+++ src/edu/mum/mpp/zayagerman/businessLogic/ClientFactory.java
@@ -1,19 +1,14 @@
 package edu.mum.mpp.zayagerman.businessLogic;
 
-import edu.mum.mpp.zayagerman.dao.ClientLeadDAOImpl;
 import edu.mum.mpp.zayagerman.entity.Client;
 import edu.mum.mpp.zayagerman.entity.ClientLead;
 import edu.mum.mpp.zayagerman.entity.ClientOpportunity;
-import edu.mum.mpp.zayagerman.service.ClientLeadDAO;
 import edu.mum.mpp.zayagerman.settings.ClientType;
 
-public class ClientFactory {
+final public class ClientFactory {
 	
-	private ClientLeadDAO dao;
 	
-    private ClientFactory() {
-    	dao = new ClientLeadDAOImpl();
-    }
+    private ClientFactory() { }
     
     public static Client createClient(ClientType type){
     	if(type.equals(ClientType.LEAD)){
